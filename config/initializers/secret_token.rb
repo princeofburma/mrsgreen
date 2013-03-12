147a671b3db3288c66ebdc7431c1903f5c8a371c
princeofburma <drosemill@gmail.com>
2013-03-12 12:33:33 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrsgreen::Application.config.secret_token = '792fbb3a05cfb42e0fc2c50d21b3ac52fe28d74c253b83a11147a1177f12107b4bc5ffee6c9f5c254dc349378cdbb2a876fb1a4c1b99eb85b25c3164170a8d91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mrsgreen::Application.config.session_store :cookie_store, key: '_mrsgreen_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mrsgreen::Application.config.session_store :active_record_store
